0273dc1a75ec96f2be246374fc5b2caf5ce59986
MissMicro <blog@missmicrogeek.pl>
2017-09-08 23:49:11 +0200

    second

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function myFunction() {
 // imie.length > 1
 // email = {@}
-// temat = {*mruweczka*}
+// temat = {*Hello*}
 // => wiadomosc => ok
 
 var name = document.getElementById ('name'); 
@@ -11,9 +11,23 @@ var topic =  document.getElementById ('topic');
 
 if (name.value.length > 1 && 
 mail.value.search("@") != -1 &&
-topic.value.toLowerCase().search("mruweczka") != -1 ){
+topic.value.toLowerCase().search("Hello") != -1 ){
 alert('ok');
-} else {
-alert('liseczek');
 }
+if(name.value.length <= 1){
+    
+    document.getElementById("name").className += " required"; 
+    
+}
+if(mail.value.search("@") == -1){
+        
+    document.getElementById("mail").className += " required"; 
+    }
+    
+    if(topic.value.toLowerCase().search("Hello") == -1){
+
+        document.getElementById("topic").className += " required"; 
+    }
+
+    
 }
